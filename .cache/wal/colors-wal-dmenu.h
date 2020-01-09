11c0ac8320e2b13303a53f488d800fe7dc07f187
Brad Heffernan <ph3onix83@gmail.com>
2020-01-10 08:44:23 +1030

    updated

--- .cache/wal/colors-wal-dmenu.h
+++
@@ -1,6 +0,0 @@
-static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#cedce1", "#181b33" },
-	[SchemeSel] = { "#cedce1", "#C37C83" },
-	[SchemeOut] = { "#cedce1", "#9FC0D3" },
-};
